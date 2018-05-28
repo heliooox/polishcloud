562a0b64153ac44f8d20528b5717ea51fa951ffb
root <root@hellfire-peninsula.warlock-den.org>
2018-05-28 15:36:54 +0200

    next

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-ala ma kota
-
+echo "[+] Update repos..."
+apt-get update
+echo "[+] Install ssh server..."
+apt-get -y install openssh-server
+echo "[+] Install strongswan..."
+apt-get -y install strongswan
