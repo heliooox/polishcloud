05b7eb2d0ae8aa9871fe7d8ffaba13569abc2cec
root <root@hellfire-peninsula.warlock-den.org>
2018-05-28 16:22:29 +0200

    pass

--- install.sh
+++ install.sh
@@ -4,4 +4,37 @@ apt-get update
 echo "[+] Install ssh server..."
 apt-get -y install openssh-server
 echo "[+] Install strongswan..."
-apt-get -y install strongswan
+apt-get -y install strongswan iptables-persistent
+echo "[+] Install pip..."
+apt-get -y install python-pip
+echo "[+] Install aws..."
+pip install awscli
+echo "[+] configure strongswan..."
+echo "[conf] enter Right External IP: "
+read RExIP
+echo "[conf] enter Right Internal subnet: "
+read RInIP
+
+#password  PSK
+
+function pass () {
+        read -s -p "[conf] enter PSK Key: " PSK1
+        echo ""
+        read -s -p "[conf] confirm PSK Key: " PSK2
+        echo ""
+        if [ $PSK1 != $PSK2 ]; then
+                echo ""
+		echo "PSK don't match - try again"
+		echo ""
+                pass
+        else
+                exit
+        fi
+}
+
+pass
+
+
+
+
+
