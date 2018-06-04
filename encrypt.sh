3c791f669dfa014836f8232e57710defac7dbfa2
heliooox <39700129+heliooox@users.noreply.github.com>
2018-06-04 12:03:33 +0200

    Delete encrypt.sh

--- encrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-Nameconst=TEST.OGRODOWA.$(date +%F_%R)
-zip -r /mnt/backup/$Nameconst /mnt/backup 
-gpg --yes --encrypt --recipient "TEST.OGRODOWA" "/mnt/backup/$Nameconst"
-cp "/mnt/backup/$Nameconst" /mnt/vpn-core/
-echo "$Nameconst" >> /var/log/polishcloud.log
-
